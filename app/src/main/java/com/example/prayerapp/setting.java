4523e244411a5acd453b4e6f8e85997a09ba48ca
AlanoudAlrashed <61060127+AlanoudAlrashed@users.noreply.github.com>
2021-03-23 02:21:50 +0300

    Menu fixed & Broken silent mode

--- app/src/main/java/com/example/prayerapp/setting.java
+++ app/src/main/java/com/example/prayerapp/setting.java
@@ -3,9 +3,12 @@ package com.example.prayerapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.AlarmManager;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,24 +18,30 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Switch;
 import android.widget.CompoundButton;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Random;
 
 public class setting extends AppCompatActivity {
     DrawerLayout drawerLayout;
    Spinner spinner1, spinner2, spinner3,spinner4;
    Button btnSubmit;
-  public static int calcMethod, asrMethod,timeMethod;
+  int calcMethod, asrMethod,timeMethod, silentMethod;
+  Calendar c = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         drawerLayout = findViewById(R.id.drawer_layout);
         btnSubmit=findViewById(R.id.btnSubmit);
-        //Create Spinner 1
+
+
+        //Create Spinner 1 --------------------------------------------------------------------------------------
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         List<String> list = new ArrayList<String>();
         list.add("اثنى عشري");
@@ -62,6 +71,8 @@ public class setting extends AppCompatActivity {
             }
         });
 
+        //Create Spinner 2 --------------------------------------------------------------------------------------
+
         spinner2 = (Spinner) findViewById(R.id.spinner2);
         List<String> list2 = new ArrayList<String>();
         list2.add("شافعي");
@@ -85,6 +96,8 @@ public class setting extends AppCompatActivity {
                 Home.asrMethod=0;
             }
         });
+
+        //Create Spinner 3 --------------------------------------------------------------------------------------
 
          spinner3 = (Spinner) findViewById(R.id.spinner3);
         List<String> list3 = new ArrayList<String>();
@@ -111,8 +124,12 @@ public class setting extends AppCompatActivity {
      }
  });
 
+        //Create Spinner 4 --------------------------------------------------------------------------------------
+
         spinner4 = (Spinner) findViewById(R.id.spinner4);
         List<String> list4 = new ArrayList<String>();
+        list4.add("نظام الرنين");
+        list4.add("غير صامت");
         list4.add("15 دقيقة");
         list4.add("30 دقيقة");
         list4.add("45 دقيقة");
@@ -123,7 +140,49 @@ public class setting extends AppCompatActivity {
         spinner4.setAdapter(dataAdapter4);
 
 
-        //Switch declerations
+        spinner4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+              silentMethod= (int) spinner4.getSelectedItemId();
+                Log.i("silent method",silentMethod+"");
+                Home.timeformat=silentMethod;
+
+                Long delay;
+
+                switch(silentMethod){
+                    //case 0: //turn off Silent
+                       // silentOFF();
+                       // Toast.makeText(getApplicationContext() , "تم إيقاف الوضع الصامت", Toast.LENGTH_LONG).show();
+                       // break;
+                    case 2:// 15 minutes 900000
+                        delay = (long) 60000;
+                        setTime(delay);
+                        Toast.makeText(getApplicationContext() , "تم تشغيل الوضع الصامت ل 15 دقيقة", Toast.LENGTH_LONG).show();
+                        break;
+                    case 3://30 minutes
+                        delay = (long) 900000*2;
+                        setTime(delay);
+                        Toast.makeText(getApplicationContext() , "تم تشغيل الوضع الصامت ل 30 دقيقة", Toast.LENGTH_LONG).show();
+                        break;
+                    case 4 : //45 minutes
+                        delay = (long) 2700000;
+                        setTime(delay);
+                        Toast.makeText(getApplicationContext() , "تم تشغيل الوضع الصامت ل 45 دقيقة", Toast.LENGTH_LONG).show();
+                        break;
+
+                    default :
+
+                }//end switch
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Home.timeformat=1;
+            }
+        });
+
+
+        //Switch declerations **************************************DELETE WHEN DONE
         Switch mySwitch = (Switch)findViewById(R.id.on_off_switch);
         mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -164,6 +223,77 @@ public class setting extends AppCompatActivity {
 
     }//End on create
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("choice",silentMethod);
+    }
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        silentMethod=savedInstanceState.getInt("choice");
+        spinner4.setSelection(silentMethod);
+    }
+
+    //Turn OFF Silent Mode -----------------------------------------------------------------------------------
+
+    public void silentOFF(){
+        //Turn of Silent
+        Context context = getApplicationContext();
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !notificationManager.isNotificationPolicyAccessGranted()) {
+
+            Intent intent = new Intent(
+                    android.provider.Settings
+                            .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+
+            startActivity(intent);
+        }
+        AudioManager myAudioManager;
+        myAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        myAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+    }//End silentOFF
+
+    //Set phone on silent for a specific time --------------------------------------------------------------------
+
+    public void setTime(long time){ //this method would call the broad cast after the desired interval
+
+        System.out.println("in setTime: "+ time);
+
+        //Set Phone on Silent ------------------------------------------------------------------------------------
+        Context context = getApplicationContext();
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !notificationManager.isNotificationPolicyAccessGranted()) {
+
+            Intent intent = new Intent(
+                    android.provider.Settings
+                            .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+
+            startActivity(intent);
+        }
+        AudioManager myAudioManager;
+        myAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        myAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+
+
+        //Set duration to turn off alarm -------------------------------------------------------------------------
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        Log.i("tag","bitch work");
+                        silentOFF();
+                    }
+                }, time);
+
+    }//end setTime
 
 
     //Menu related methods
