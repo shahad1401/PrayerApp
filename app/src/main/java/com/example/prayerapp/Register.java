3fc2dfa5e96074ceb4ac39cd47bc2259c1039a85
shahad1401 <shahadalsweed@gmail.com>
2021-03-28 03:50:40 +0300

    email format

--- app/src/main/java/com/example/prayerapp/Register.java
+++ app/src/main/java/com/example/prayerapp/Register.java
@@ -10,6 +10,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Register extends AppCompatActivity {
 
     EditText email,pass,confpass;
@@ -45,30 +48,41 @@ public class Register extends AppCompatActivity {
                 String pwd = pass.getText().toString().trim();
                 String cpwd = confpass.getText().toString().trim();
 
-                if (db.checkExist(user)){
-                    Toast.makeText(Register.this,"already exist",Toast.LENGTH_SHORT).show();
-                }else {
-                    if (pwd.length() < 8){
-                        Toast.makeText(Register.this,"short password",Toast.LENGTH_SHORT).show();
+                if (!isEmail(user)){
+                    Toast.makeText(Register.this,"not email format",Toast.LENGTH_SHORT).show();
+                } else {
+                    if (db.checkExist(user)){
+                        Toast.makeText(Register.this,"already exist",Toast.LENGTH_SHORT).show();
                     }else {
-                        if(pwd.equals(cpwd)){
-                            long val = db.addUser(user,pwd);
-                            if(val > 0){
-                                Toast.makeText(Register.this,"You have registered",Toast.LENGTH_SHORT).show();
-                                Intent moveToLogin = new Intent(Register.this,Login.class);
-                                startActivity(moveToLogin);
+                        if (pwd.length() < 8){
+                            Toast.makeText(Register.this,"short password",Toast.LENGTH_SHORT).show();
+                        }else {
+                            if(pwd.equals(cpwd)){
+                                long val = db.addUser(user,pwd);
+                                if(val > 0){
+                                    Toast.makeText(Register.this,"You have registered",Toast.LENGTH_SHORT).show();
+                                    Intent moveToLogin = new Intent(Register.this,Login.class);
+                                    startActivity(moveToLogin);
+                                }
+                                else{
+                                    Toast.makeText(Register.this,"Registeration Error",Toast.LENGTH_SHORT).show();
+                                }
+
                             }
                             else{
-                                Toast.makeText(Register.this,"Registeration Error",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(Register.this,"Password is not matching",Toast.LENGTH_SHORT).show();
                             }
-
-                        }
-                        else{
-                            Toast.makeText(Register.this,"Password is not matching",Toast.LENGTH_SHORT).show();
                         }
                     }
                 }
             }
         });
+    }
+
+    public boolean isEmail(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
